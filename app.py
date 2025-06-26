067dbd8e259a26a5ddaf6cd855cb97dab8f3194d
Rohan <rohanbattepati20@gmail.com>
2025-06-26 12:23:37 +0530

    Removed unused Flask-Mail setup

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
-mail = Mail(app)
+
 
 app.secret_key = os.getenv('SECRET_KEY')
 
